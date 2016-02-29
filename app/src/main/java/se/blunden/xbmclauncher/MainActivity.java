c0a46b5fe605ac3447a9dcadbf5f545d1e98cc01
blunden <blunden2@gmail.com>
2016-02-29 22:49:19 +0100

    Fix the issue with BOOT_COMPLETED not being sent when set as launcher

--- app/src/main/java/se/blunden/xbmclauncher/MainActivity.java
+++ app/src/main/java/se/blunden/xbmclauncher/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -23,16 +22,15 @@ public class MainActivity extends Activity {
 		
 		xbmcActivity = settings.getString("xbmc_variant", getString(R.string.xbmc_activity_default));
 		
-		Intent activityIntent;
-		
-		// Intent.makeMainActivity is only available in API level 11 and above
-		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			activityIntent = Intent.makeMainActivity(ComponentName.unflattenFromString(xbmcActivity));
-		} else {
-			activityIntent = new Intent(Intent.ACTION_MAIN);
-            activityIntent.setComponent(ComponentName.unflattenFromString(xbmcActivity));
-            activityIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-		}
+		// Build the Kodi/XBMC intent
+        Intent activityIntent;
+        activityIntent = new Intent(Intent.ACTION_MAIN);
+        activityIntent.setComponent(ComponentName.unflattenFromString(xbmcActivity));
+        /*
+         * The launched activity will be considered the HOME activity and will be on top of the
+         * stack as needed for the system to broadcast BOOT_COMPLETED.
+         */
+        activityIntent.addCategory(Intent.CATEGORY_HOME);
 		
 		try {
 			startActivity(activityIntent);

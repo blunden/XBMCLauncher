4df39ed72f77fc7da072775990a471692146354f
blunden <blunden2@gmail.com>
2023-11-01 22:19:08 +0100

    Add support for launching arbitrary user specified Kodi forks
    It's a bit cumbersome, but apps are not able to retrieve details about other
    apps since Android 11 without a special permission that requires special Play
    Store approval.

--- app/src/main/java/se/blunden/xbmclauncher/LauncherSettingsActivity.java
+++ app/src/main/java/se/blunden/xbmclauncher/LauncherSettingsActivity.java
@@ -2,7 +2,9 @@ package se.blunden.xbmclauncher;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
 
 public class LauncherSettingsActivity extends Activity {
 	@Override
@@ -23,6 +25,20 @@ public class LauncherSettingsActivity extends Activity {
 
 	        // Load the preferences from XML
 	        addPreferencesFromResource(R.xml.preferences);
+
+			EditTextPreference customVariantPref = (EditTextPreference) findPreference("xbmc_custom_variant");
+			customVariantPref.setSummary(getPreferenceManager().getSharedPreferences().getString("xbmc_custom_variant", ""));
+			customVariantPref.setOnPreferenceChangeListener((preference, newValue) -> {
+				customVariantPref.setSummary((String) newValue);
+				return true;
+			});
+
+			customVariantPref.setEnabled(getPreferenceManager().getSharedPreferences().getBoolean("override_xbmc_variant", false));
+			SwitchPreference customVariantTogglePref = (SwitchPreference) findPreference("override_xbmc_variant");
+			customVariantTogglePref.setOnPreferenceChangeListener((preference, newValue) -> {
+				customVariantPref.setEnabled((Boolean) newValue);
+				return true;
+			});
 	    }   
 	}
 }
--- app/src/main/java/se/blunden/xbmclauncher/MainActivity.java
+++ app/src/main/java/se/blunden/xbmclauncher/MainActivity.java
@@ -8,6 +8,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends Activity {
 	
@@ -15,11 +19,22 @@ public class MainActivity extends Activity {
 
     private void launch() {
 		// Load preferences
-		SharedPreferences settings;
-		settings = getSharedPreferences("se.blunden.xbmclauncher_preferences", Context.MODE_PRIVATE);
+		SharedPreferences settings = getSharedPreferences("se.blunden.xbmclauncher_preferences", Context.MODE_PRIVATE);
+		String xbmcActivity = settings.getString("xbmc_variant", getString(R.string.xbmc_activity_default));
 
-        String xbmcActivity = settings.getString("xbmc_variant", getString(R.string.xbmc_activity_default));
-		
+		if (settings.getBoolean("override_xbmc_variant", false) && !settings.getString("xbmc_custom_variant", "").isEmpty()) {
+			String customActivityString = settings.getString("xbmc_custom_variant", getString(R.string.xbmc_activity_default));
+			if (customActivityString != null && !customActivityString.isEmpty() && validateActivityString(customActivityString)) {
+				xbmcActivity = customActivityString;
+			} else {
+				Toast.makeText(this, "Entered custom activity has an invalid format", Toast.LENGTH_SHORT).show();
+				Log.d(TAG, "Failed to find suitable launch activity for specified package name");
+
+				launchSettings();
+				return;
+			}
+		}
+
 		// Build the Kodi/XBMC intent
         Intent activityIntent;
         activityIntent = new Intent(Intent.ACTION_MAIN);
@@ -30,7 +45,7 @@ public class MainActivity extends Activity {
          */
         activityIntent.addCategory(Intent.CATEGORY_HOME);
         activityIntent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-		
+
 		try {
 			startActivity(activityIntent);
         }
@@ -55,5 +70,13 @@ public class MainActivity extends Activity {
 	private void launchSettings() {
 		Intent launchSettings = new Intent(this, LauncherSettingsActivity.class);
 		startActivity(launchSettings);
+	}
+
+	private static boolean validateActivityString(String activityString) {
+		// Try to make a rough regex validation of the provided activity string
+		final Pattern pattern = Pattern.compile("[A-Za-z0-9]+\\.[A-Za-z0-9]+\\.[A-Za-z0-9]+/[A-Za-z0-9]+\\.[A-Za-z0-9]+\\.[A-Za-z0-9]+\\.[A-Za-z0-9]+", Pattern.CASE_INSENSITIVE);
+		final Matcher matcher = pattern.matcher(activityString);
+
+		return matcher.matches();
 	}
 }
